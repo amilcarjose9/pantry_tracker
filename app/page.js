f62acd427eda64a87021d8c9c28619803afdf3df
amilcarjose9 <amilcarjpp@gmail.com>
2024-08-02 15:12:47 -0400

    Add search item functionality

--- app/page.js
+++ app/page.js
@@ -7,8 +7,10 @@ import { collection, deleteDoc, doc, getDocs, query, setDoc, getDoc} from "fireb
 
 export default function Home() {
   const[inventory, setInventory] = useState([])
-  const[open, setOpen] = useState(false)
+  const[openAdd, setOpenAdd] = useState(false)
+  const [openSearch, setOpenSearch] = useState(false);
   const[itemName, setItemName] = useState('')
+  const [searchResult, setSearchResult] = useState(null);
 
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'inventory'))
@@ -57,12 +59,24 @@ export default function Home() {
     await updateInventory()
   }
 
+  const searchItem = async (item) => {
+    const foundItem = inventory.find(invItem => invItem.name === item)
+    if (foundItem) {
+      setSearchResult({ name: foundItem.name, quantity: foundItem.quantity })
+    } else {
+      setSearchResult({ name: item, message: `${item.charAt(0).toUpperCase() + item.slice(1)} not found`})
+    }
+  };
+
   useEffect(()=>{
     updateInventory()
   }, [])
 
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
+  const handleOpenAdd = () => setOpenAdd(true)
+  const handleCloseAdd = () => setOpenAdd(false)
+
+  const handleOpenSearch = () => setOpenSearch(true)
+  const handleCloseSearch = () => setOpenSearch(false)
  
   return (
     <Box 
@@ -74,7 +88,7 @@ export default function Home() {
       alignItems="center" 
       gap={2}
     >
-      <Modal open={open} onClose={handleClose}>
+      <Modal open={openAdd} onClose={handleCloseAdd}>
         <Box
           position="absolute"
           top="50%"
@@ -104,9 +118,9 @@ export default function Home() {
             <Button
               variant="outlined"
               onClick={() => {
-                addItem(itemName)
+                addItem(itemName.toLocaleLowerCase())
                 setItemName('')
-                handleClose()
+                handleCloseAdd()
               }}
             >
               Add
@@ -114,14 +128,107 @@ export default function Home() {
           </Stack>
         </Box>
       </Modal>
-      <Button 
-        variant="contained" 
-        onClick={()=> {
-          handleOpen()
-        }}
-      >
-        Add New Item
-      </Button>
+
+      <Modal open={openSearch} onClose={handleCloseSearch}>
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{
+            transform: "translate(-50%,-50%)"
+          }}
+        >
+          <Typography variant="h6">Search Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => {
+                setItemName(e.target.value)
+              }}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                searchItem(itemName.toLowerCase());
+                setItemName('')
+              }}
+            >
+              Search
+            </Button>
+          </Stack>
+              {searchResult && (
+                <Box display="flex" flexDirection="column" alignItems="center" mt={2}>
+                  {searchResult.message ? (
+                    <Typography variant="h5" color="#333" textAlign="center">
+                      {searchResult.message}
+                    </Typography>
+                    ) : (
+                      <>
+                        <Stack direction="row" spacing={2}>
+                          <Typography variant="h5" color="#333" textAlign="center">
+                            {searchResult.name.charAt(0).toUpperCase() + searchResult.name.slice(1)}
+                          </Typography>
+                          <Typography variant="h5" color="#333" textAlign="center">
+                            {searchResult.quantity}
+                          </Typography>
+                          
+                            <Button 
+                              variant="contained" 
+                              onClick={async () => {
+                                addItem(searchResult.name)
+                                setSearchResult({ ...searchResult, quantity: searchResult.quantity + 1 })
+                              }}
+                            >
+                              Add
+                            </Button>
+                            <Button 
+                              variant="contained" 
+                              onClick={async () => {
+                                removeItem(searchResult.name)
+                                setSearchResult(prev => prev.quantity > 1 
+                                  ? { ...prev, quantity: prev.quantity - 1 } 
+                                  : { name: searchResult.name, message: `${searchResult.name.charAt(0).toUpperCase() + searchResult.name.slice(1)} not found`})
+                              }}
+                            >
+                              Remove
+                            </Button>
+                        </Stack>
+                      </>
+                  )}
+               </Box>
+              )}
+        </Box>
+      </Modal>  
+
+      <Stack direction="row" spacing={2}>
+        <Button 
+          variant="contained" 
+          onClick={()=> {
+            handleOpenAdd()
+          }}
+        >
+          Add New Item
+        </Button>
+        <Button 
+          variant="contained" 
+          onClick={()=> {
+            handleOpenSearch()
+          }}
+        >
+          Search Item
+        </Button>
+      </Stack>
+
       <Box border="1px solid #333">
         <Box
           width="800px"
@@ -159,6 +266,9 @@ export default function Home() {
                     variant="contained" 
                     onClick={() => {
                       addItem(name)
+                      if (searchResult?.name === name) {
+                        setSearchResult({ ...searchResult, quantity: searchResult.quantity + 1 });
+                      }
                     }}
                   >
                     Add
@@ -167,6 +277,11 @@ export default function Home() {
                     variant="contained" 
                     onClick={() => {
                       removeItem(name)
+                      if (searchResult?.name === name) {
+                        setSearchResult(prev => prev.quantity > 1 
+                          ? { ...prev, quantity: prev.quantity - 1 } 
+                          : { name: searchResult.name, message: `${searchResult.name.charAt(0).toUpperCase() + searchResult.name.slice(1)} not found`})
+                      }
                     }}
                   >
                     Remove
@@ -175,7 +290,6 @@ export default function Home() {
               </Box>
             ))
           }
-
         </Stack>
       </Box>
 
